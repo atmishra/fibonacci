e35aec7d8335d359f37d7611db3b3da4d0660515
atmishra <at.mishra007@gmail.com>
2016-09-08 10:42:43 +0530

    created module for getting fibbonaci term

---
+++ fibbonaci_term/getterm.py
@@ -0,0 +1,6 @@
+def term(n):
+
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
--- fibbonaci_term/views.py
+++ fibbonaci_term/views.py
@@ -2,41 +2,24 @@
 from django.template.response import TemplateResponse
 
 from .forms import InputForm
-# from mymodules import getterm
+
+from .getterm import term
 
 
 def index(request):
-    if request.method == 'POST':
-        form = InputForm(request.POST)
 
+    form = InputForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+
+    if request.method == 'POST':
         if form.is_valid():
 
             n = form.cleaned_data['n']
             fib_term = term(n)
 
-            context = {
-                'n': n,
-                'term': fib_term,
-                'form': form,
+            context.update({'n': n, 'term': fib_term})
 
-            }
-
-    elif request.method == 'GET':
-
-        form = InputForm()
-
-        context = {
-
-
-            'form': form,
-
-        }
     return TemplateResponse(request, 'pages/home.html', context)
 
-
-def term(n):
-
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-    return a
